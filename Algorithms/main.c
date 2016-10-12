d17fcbf3e862e614d985c1e287022e010ed6b26e
mangonob <534393806@qq.com>
2016-10-12 21:45:10 +0800

    Initial Commit

---
+++ Algorithms/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algorithms
+//
+//  Created by Trinity on 16/10/12.
+//  Copyright © 2016年 Trinity. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
